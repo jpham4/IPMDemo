c45e24cfba5a688e236e4747cc0e67dc3ac0dfa6
eychcue <hq.haroon1@gmail.com>
2018-04-26 16:22:52 -0400

    fixed userID graph issue

--- source/wothdata.php
+++ source/wothdata.php
@@ -1,12 +1,24 @@
 <?php include ('authenticate.php');
+// if (!isLoggedIn()) {
+//     $_SESSION['msg'] = "You must log in first";
+//     header('location: login.php');
+// }
 //setting header to json
 header('Content-Type: application/json');
 
 //query to get data from the table
 // $query = sprintf("SELECT userid, facebook, twitter, googleplus FROM followers");
 
+if (isset($_SESSION['user'])){
+$userID = $_SESSION['user']['id'];
+}else {
+$userID = '0';
+}
+
+
+
 //$userID = mysqli_query("SELECT id FROM Account WHERE user_type='users'" .$_SESSION['id']);
-$query = sprintf("SELECT date_posted, worth from portfolioHistory where portfolioID = 5");
+$query = sprintf("SELECT date_posted, worth from portfolioHistory where portfolioID = ".$userID);
 
 //execute query
 $result = $db->query($query);
@@ -25,3 +37,7 @@ $db->close();
 
 //now print the data
 print json_encode($data);
+// print (String)$userID;
+// print "hellooooo";
+// print (int)$userID;
+?>
